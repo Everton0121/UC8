1d41a0f4a6b133d4148ebc06816a3d7a7143c20a
eduuuu <everton.edu.2009@gmail.com>
2025-06-04 21:36:00 -0300

    Add files via upload

---
+++ eventos/Eventos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uc9.eventos;
+
+/**
+ *
+ * @author EDUARDOEVERTONRODRIG
+ */
+public class Eventos {
+
+    public static void main(String[] args) {
+
+    }
+}
